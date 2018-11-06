199bbda22d337fde0c5cb82d15393a4d8b42a302
lily <lily@topband.com.cn>
2018-11-06 09:46:08 +0800

    修改FirstClass

--- git-test-project/src/com/topband/git/FirstClass.java
+++ git-test-project/src/com/topband/git/FirstClass.java
@@ -8,10 +8,12 @@ public class FirstClass {
 		System.out.println("hello git project");
 		System.out.println("hello git project");
 		System.out.println("hello git project");
+		//hello test
 	}
 
 	public void test() {
 		int i = 0;
 		i += 10;
+		System.out.println(i);
 	}
 }
