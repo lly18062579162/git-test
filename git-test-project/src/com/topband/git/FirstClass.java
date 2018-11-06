6b16c640c7b66e4a93b9ed5a636c3511adc896e7
lily <lily@topband.com.cn>
2018-11-06 09:33:32 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/topband/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.topband.git;
+
+public class FirstClass {
+	public static void main(String[] args) {
+		System.out.println("hello git project");
+		System.out.println("hello git project");
+		System.out.println("hello git project");
+		System.out.println("hello git project");
+		System.out.println("hello git project");
+		System.out.println("hello git project");
+	}
+}
